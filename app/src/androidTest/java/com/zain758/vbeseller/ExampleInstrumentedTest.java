de2d26b17a8f45f4062dedfbc539636fb5236f68
Zain <zainulabideen758@gmail.com>
2020-12-20 21:54:46 +0500

    VBE-seller

--- app/src/androidTest/java/com/zain758/vbeseller/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/zain758/vbeseller/ExampleInstrumentedTest.java
@@ -21,6 +21,5 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.zain758.vbeseller", appContext.getPackageName());
     }
 }
